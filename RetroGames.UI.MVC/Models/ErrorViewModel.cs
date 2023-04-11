92e7bd1b9aa8d691b53c4feb00e312c879737104
Jason Pinkston <jason.pinkston.it@outlook.com>
2023-04-11 15:08:19 -0500

    Add project files.

---
+++ RetroGames.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RetroGames.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
