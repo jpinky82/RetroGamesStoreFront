e107af9c9e07454f498dbdf50883c8c441319a93
Jason Pinkston <jason.pinkston.it@outlook.com>
2023-04-12 21:29:20 -0500

    Added Metadata, somehow my root folder was deleted so that was readded back in, and I customized the edit, create, delete, details, and index pages for all my views.

---
+++ RetroGames.DATA.EF/Metadata/Metadata.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RetroGames.DATA.EF.Models//.Metadata
+{
+	//internal class Metadata
+	//{
+	//}
+
+	public class CategoryMetadata
+	{
+		public int CategoryId { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(50)]
+		[Display(Name = "Category")]
+		public string CategoryName { get; set; } = null!;
+
+		[StringLength(500)]
+		[Display(Name = "Description")]
+		[DisplayFormat(NullDisplayText="n/a")]
+		public string? CategoryDescription { get; set; }
+	}
+
+	public class ConsoleTypeMetadata
+	{
+		public int ConsoleTypeId { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(50)]
+		[Display(Name = "Console")]
+		public string ConsoleName { get; set; } = null!;
+	}
+
+	public class GenreMetadata
+	{
+		public int GenreId { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(50)]
+		[Display(Name = "Genre")]
+		public string GenreName { get; set; } = null!;
+
+		[StringLength(500)]
+		[Display(Name = "Description")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? GenreDescription { get; set; }
+	}
+	public class ManufacturerMetadata
+	{
+		public int ManufacturerId { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(200)]
+		[Display(Name = "Manufacturer")]
+		public string ManufacturerName { get; set; } = null!;
+
+		[StringLength(150)]
+		[Display(Name = "Address")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? ManAddress { get; set; }
+
+		[StringLength(50)]
+		[Display(Name = "City")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? ManCity { get; set; }
+
+		[StringLength(2)]
+		[Display(Name = "State")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? ManState { get; set; }
+
+		[StringLength(5)]
+		[Display(Name = "Zip Code")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? ManZip { get; set; }
+
+		[StringLength(24)]
+		[Display(Name = "Phone")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? ManPhone { get; set; }
+
+	}
+	public class OrderMetadata
+	{
+		public int OrderId { get; set; }
+		public string UserId { get; set; } = null!;
+
+		[Required(ErrorMessage = "Date is Required")]
+		[Display(Name = "Date")]
+		[DisplayFormat(DataFormatString ="{0:d}, ApplyFormatInEditMode=true")]
+		public DateTime OrderDate { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(150)]
+		[Display(Name = "Name")]
+		public string ShipToName { get; set; } = null!;
+
+		[Required(ErrorMessage = "City is required")]
+		[StringLength(50)]
+		[Display(Name = "City")]
+		public string ShipCity { get; set; } = null!;
+
+		[StringLength(2)]
+		[Display(Name = "State")]
+		public string? ShipState { get; set; }
+
+		[Required(ErrorMessage = "Zip Code is Required")]
+		[StringLength(5)]
+		[Display(Name = "Zip Code")]
+		public string ShipZip { get; set; } = null!;
+	}
+	public class OrderProductMetadata
+	{
+		public int OrderProductId { get; set; }
+		public int ProductId { get; set; }
+		public int OrderId { get; set; }
+
+		[Range(0, (double)(short.MaxValue))]
+		public short? Quantity { get; set; }
+
+		[Range(0, (double)(decimal.MaxValue))]
+		[Display(Name = "Price")]
+		[DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:c}")]
+		public decimal? ProductPrice { get; set; }
+	}
+	public class ProductMetadata
+	{
+		public int ProductId { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(200)]
+		[Display(Name = "Name")]
+		public string ProductName { get; set; } = null!;
+
+		[Required(ErrorMessage = "Price is Required")]
+		[Range(0, (double)(decimal.MaxValue))]
+		[Display(Name = "Price")]
+		[DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:c}")]
+		public decimal ProductPrice { get; set; }
+
+		[Display(Name = "Date")]
+		[DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode=true)]
+		public DateTime? ReleaseDate { get; set; }
+
+		[StringLength(500)]
+		[Display(Name = "Description")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? ProductDescription { get; set; }
+
+		[Required(ErrorMessage = "Units In Stock is Required")]
+		[Range(0, (double)(short.MaxValue))]
+		[Display(Name = "Stock")]
+		public short UnitsInStock { get; set; }
+		public int? ConsoleTypeId { get; set; }
+		public int ManufacturerId { get; set; }
+		public int CategoryId { get; set; }
+		public int? GenreId { get; set; }
+
+		[Display(Name = "Discontinued?")]
+		public bool IsDiscontinued { get; set; }
+
+		[StringLength(75)]
+		[Display(Name = "Image")]
+		public string? ProductImage { get; set; }
+	}
+	public class UserMetadata
+	{
+		public string UserId { get; set; } = null!;
+
+		[Required(ErrorMessage = "First Name is required")]
+		[StringLength(50)]
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; } = null!;
+
+		[Required(ErrorMessage = "Last Name is required")]
+		[StringLength(50)]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; } = null!;
+
+		[StringLength(150)]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? Address { get; set; }
+
+		[StringLength(50)]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? City { get; set; }
+
+		[StringLength(2)]
+		public string? State { get; set; }
+
+		[StringLength(5)]
+		[Display(Name = "Zip Code")]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? Zip { get; set; }
+
+		[StringLength(24)]
+		[DisplayFormat(NullDisplayText = "n/a")]
+		public string? Phone { get; set; }
+	}
+
+}
---
+++ RetroGames.UI.MVC/Controllers/ConsoleTypesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RetroGames.DATA.EF.Models;
+
+namespace RetroGames.UI.MVC.Controllers
+{
+    public class ConsoleTypesController : Controller
+    {
+        private readonly RetroGamesContext _context;
+
+        public ConsoleTypesController(RetroGamesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ConsoleTypes
+        public async Task<IActionResult> Index()
+        {
+              return _context.ConsoleTypes != null ? 
+                          View(await _context.ConsoleTypes.ToListAsync()) :
+                          Problem("Entity set 'RetroGamesContext.ConsoleTypes'  is null.");
+        }
+
+        // GET: ConsoleTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ConsoleTypes == null)
+            {
+                return NotFound();
+            }
+
+            var consoleType = await _context.ConsoleTypes
+                .FirstOrDefaultAsync(m => m.ConsoleTypeId == id);
+            if (consoleType == null)
+            {
+                return NotFound();
+            }
+
+            return View(consoleType);
+        }
+
+        // GET: ConsoleTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ConsoleTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ConsoleTypeId,ConsoleName")] ConsoleType consoleType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(consoleType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consoleType);
+        }
+
+        // GET: ConsoleTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ConsoleTypes == null)
+            {
+                return NotFound();
+            }
+
+            var consoleType = await _context.ConsoleTypes.FindAsync(id);
+            if (consoleType == null)
+            {
+                return NotFound();
+            }
+            return View(consoleType);
+        }
+
+        // POST: ConsoleTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ConsoleTypeId,ConsoleName")] ConsoleType consoleType)
+        {
+            if (id != consoleType.ConsoleTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(consoleType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConsoleTypeExists(consoleType.ConsoleTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consoleType);
+        }
+
+        // GET: ConsoleTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ConsoleTypes == null)
+            {
+                return NotFound();
+            }
+
+            var consoleType = await _context.ConsoleTypes
+                .FirstOrDefaultAsync(m => m.ConsoleTypeId == id);
+            if (consoleType == null)
+            {
+                return NotFound();
+            }
+
+            return View(consoleType);
+        }
+
+        // POST: ConsoleTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ConsoleTypes == null)
+            {
+                return Problem("Entity set 'RetroGamesContext.ConsoleTypes'  is null.");
+            }
+            var consoleType = await _context.ConsoleTypes.FindAsync(id);
+            if (consoleType != null)
+            {
+                _context.ConsoleTypes.Remove(consoleType);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConsoleTypeExists(int id)
+        {
+          return (_context.ConsoleTypes?.Any(e => e.ConsoleTypeId == id)).GetValueOrDefault();
+        }
+    }
+}
